26da5262904bc75687062869f95881fdf08b8391
tarig elamin <telamin@chewy.com>
2022-07-18 13:55:53 -0400

    adding changes

--- components/card.js
+++ components/card.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+import { useState } from 'react';
 
 
 
-const card = (props) => {
+
+
+const card = () => {
+  
+
   return (
-    <div>
-      <Paper variant='elevation' className='paper' >
-        <Typography color="primary" variant="h6">Servics : {props.name}</Typography>
-        <Typography color="primary" variant="h6">health : {props.health}</Typography>
-        <Typography color="primary" variant="h6">independencies : {props.children}</Typography>
-
+      <Paper variant='elevation' className='paper' sx={{fontFamily:"roboto",display:"flex",alignItems:"center"}} >
+        <Typography color="primary" variant="small" sx={{color:"black",flexGrow:1.5,marginLeft:2}}>Postgresql </Typography>
+        <Typography color="primary" variant="small" sx={{color:"black",flexGrow:1}}>Https <br/> <Rating defaultValue={8} max={8} readOnly={true}/> </Typography>
+        <Typography color="primary" variant="small" sx={{color:"black",flexGrow:1}}>Servelt<br/> <Rating defaultValue={8} max={8} readOnly={true}/> </Typography>
       </Paper>
-    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
-// import Head from 'next/head'
 import Hader from '../components/hader'
-// import Footer from '../components/footer'
-import TreeViw from '../components/tree'
-// import FakeAPI from '../components/fakeAPI'
-// import Api from '../components/Api'
 import TreeView from '@mui/lab/TreeView';
 import TreeItem from '@mui/lab/TreeItem';
 import Card from '../components/card';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from 'react';
+
 
 
 export async function getStaticProps() {
-  const res = await fetch ("http://localhost:8000/services")
+  const res = await fetch ("http://localhost:8000/sources")
   const data = await res.json()
   return{
     props:{
@@ -20,92 +17,13 @@ export async function getStaticProps() {
     }
   }
  }
-//  export const seasons = [
-//   {
-//     id: 1,
-//     name: "Seasons",
-//     children: [
-//       {
-//         id: 2,
-//         name: "Summer",
-//         children: [
-//           {
-//             id: 3,
-//             name: "Hot",
-//             children: [
-//               {
-//                 id: 4,
-//                 name: "June",
-//                 route: "/June"
-//               },
-//               {
-//                 id: 5,
-//                 name: "July",
-//                 route: "/July"
-//               }
-//             ]
-//           },
 
-//           {
-//             id: 6,
-//             name: "August",
-//             route: "/August"
-//           }
-//         ]
-//       },
-//       {
-//         id: 7,
-//         name: "Fall",
-//         children: [
-//           {
-//             id: 8,
-//             name: "September",
-//             route: "/September"
-//           },
-//           {
-//             id: 9,
-//             name: "October",
-//             route: "/October"
-//           },
-//           {
-//             id: 10,
-//             name: "November",
-//             route: "/November"
-//           }
-//         ]
-//       },
-//       {
-//         id: 11,
-//         name: "Winter",
-//         children: [
-//           {
-//             id: 12,
-//             name: "December",
-//             route: "/December"
-//           },
-//           {
-//             id: 13,
-//             name: "January",
-//             route: "/January"
-//           },
-//           {
-//             id: 14,
-//             name: "February",
-//             route: "/February"
-//           }
-//         ]
-//       }
-//     ]
-//   }
-// ];
 const getTreeItemFromData = (data) => {
   return data.map((dataItems) => {
     let children = undefined;
-    console.log(dataItems)
     if (dataItems.children && dataItems.children.length > 0 ){
       children = getTreeItemFromData (dataItems.children)
     }
-    console.log(dataItems)
     return (
       <TreeItem
             collapseIcon={<ArrowDropDownIcon/>}
@@ -114,11 +32,13 @@ const getTreeItemFromData = (data) => {
             nodeId={dataItems.id}
             label={dataItems.name}
             children={children}
+            onClick={showCard}
           />
     )
     }
   )
 }
+
 const DataTreeView = ({ data }) => {
   return (
     <TreeView className='treeview'>
@@ -129,11 +49,16 @@ const DataTreeView = ({ data }) => {
   );
 };
 
+
+
 export default function Home({data}) {
   return (
-  <div>
-    <Hader/>
+    <div>
+    <Hader/> 
+  <div className="flex-container">
     <DataTreeView data={data}/>
+    
    </div> 
+   </div>
   )
 }
